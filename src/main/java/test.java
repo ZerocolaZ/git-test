8303577237539757098f52a83cf88eabf6cbaa65
张旭鹏 <1050545481@qq.com>
2021-07-18 22:12:20 +0800

    IDEA_FIRST_COMMIT

---
+++ src/main/java/test.java
@@ -0,0 +1,9 @@
+/**
+ * @author shkstart
+ * @create 2021-07-18 22:10
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello~ IDEA_git");
+    }
+}
